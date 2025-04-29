d2de03a9fda7756062a8d45433f08020792281e7
kangminhyuk1111 <rkdglqkr12@naver.com>
2025-04-29 11:00:04 +0900

    feat: 문자열 계산기 3단계 구현 완료

--- src/main/java/stringcalculator/BasicStringCalculator.java
+++ src/main/java/stringcalculator/BasicStringCalculator.java
@@ -1,20 +1,20 @@
 package stringcalculator;
 
-import stringcalculator.util.StringParser;
+import stringcalculator.util.InputParser;
 
 import java.util.Arrays;
 
 public class BasicStringCalculator implements StringCalculator {
 
-    private final StringParser stringParser;
+    private final InputParser inputParser;
 
-    public BasicStringCalculator(final StringParser stringParser) {
-        this.stringParser = stringParser;
+    public BasicStringCalculator(final InputParser inputParser) {
+        this.inputParser = inputParser;
     }
 
     @Override
     public int add(final String str) {
-        int[] numbers = stringParser.extractNumberFromInput(str);
+        int[] numbers = inputParser.extractNumberFromInput(str);
 
         return Arrays.stream(numbers).sum();
     }
--- src/test/java/stringcalculator/BasicStringCalculatorTest.java
+++ src/test/java/stringcalculator/BasicStringCalculatorTest.java
@@ -1,22 +1,85 @@
 package stringcalculator;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import stringcalculator.util.StringParser;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import stringcalculator.util.InputParser;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class BasicStringCalculatorTest {
 
-    private final StringCalculator sc = new BasicStringCalculator(new StringParser());
+    private final StringCalculator sc = new BasicStringCalculator(new InputParser());
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({
+            "'1,2,3', 6",
+            "'1,2,3,4,5,6', 21",
+            "'3,7,8,10,1,3', 32"
+    })
     @DisplayName("값 입력시 올바른 반환")
-    void addString() {
-        final String input = "1,2,3";
-
+    void addString(String input, int answer) {
         final int result = sc.add(input);
 
-        assertEquals(result, 6);
+        assertEquals(result, answer);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'1:2:3', 6",
+            "'1,2,3:4:5:6', 21",
+            "'3,7:8:10,1,3', 32"
+    })
+    @DisplayName("값 입력시 올바른 반환 + 콜론 구분자 추가")
+    void addStringColon(String input, int answer) {
+        final int result = sc.add(input);
+
+        assertEquals(result, answer);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'', 0",
+    })
+    @DisplayName("빈 문자열 입력시 0 반환")
+    void blankString(String input, int answer) {
+        final int result = sc.add(input);
+
+        assertEquals(result, answer);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'-1,-2,-3'","'10,20:30,-40'"
+    })
+    @DisplayName("음수 입력시 RuntimeException 발생")
+    void StringNegativeException(String input) {
+        assertThrows(RuntimeException.class, () -> sc.add(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'t,-2,-3'",
+            "'1,2,3:tyy'"
+    })
+    @DisplayName("숫자가 아닌 값 입력시 RuntimeException 발생")
+    void StringIsNotValidException(String input) {
+        assertThrows(RuntimeException.class, () -> sc.add(input));
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "'//;\n1;2;3', 6",
+            "'//;\n4;5:6', 15",
+            "'//;\n0,2:1;4', 7",
+            "'//;\n1;1', 2",
+    })
+    @DisplayName("구분자 존재시 구분자를 포함한 문자열 계산")
+    void addCustomDelimiter(String input, int answer) {
+        final int result = sc.add(input);
+
+        assertEquals(result, answer);
     }
 }
